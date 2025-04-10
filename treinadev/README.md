20ed09861ecd18b18b79277c0844191657654f32
FeLiPeOLi7 <felpzito999@gmail.com>
2025-04-10 12:00:33 -0300

    Criando um repositorio privado para o treina dev

--- treinadev/README.md
+++
@@ -1,1 +0,0 @@
-Projetos feitos na seletiva do curso treina dev da Campus Code em Ruby (Turma 13 - 2024).
--- treinadev/oo-bank-accounts-initialize/lib/bank_account.rb
+++
@@ -1,9 +0,0 @@
-class BankAccount
-    attr_accessor :name, :number, :balance
-
-    def initialize(name:, number:, balance: 0)
-        @name = name
-        @number = number
-        @balance = balance
-    end
-end
