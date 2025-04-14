e3792794d942a6616239fe0eac0744152b63e78b
FeLiPeOLi7 <felpzito999@gmail.com>
2025-04-14 14:34:23 -0300

    Mudancas em flag.py

--- flag/flag.py
+++ flag/flag.py
@@ -1,9 +1,12 @@
 import requests
-from bs4 import BeautifulSoup as bs
+#from bs4 import BeautifulSoup as bs
 
 flag = input("Put a flag of a country: ")
-url = "https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&"+'q='+flag
+url = "https://restcountries.com/v3.1/name/"+flag
+
 r = requests.get(url)
-soup = bs(r.content, 'html.parser')
-flagImage = soup.find_all('img', {'class': 'rg_i Q4LuWd'})['scr']
-print(flagImage)
+data = r.json()
+#soup = bs(r.content, 'html.parser')
+
+flagImage = data[0]["flags"]["png"]
+print("Flag URL: ", flagImage)
